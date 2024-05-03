2802a16233648929f6542743cc32406d42b66c43
Bruno Alves <93.bruno@gmail.com>
2024-05-03 21:02:06 +0100

    testing primeReact Header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
+import { Route } from "wouter";
 import ExercisesList from "./components/ExercisesList";
+import Header from "./components/Header";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
-  return <ExercisesList />;
+  return (
+    <>
+      <ScrollToTop />
+      <Header />
+      <Route path="/exercises" component={ExercisesList} />
+    </>
+  );
 }
 
 export default App;
