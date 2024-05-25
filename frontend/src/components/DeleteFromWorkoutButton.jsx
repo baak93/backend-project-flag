7fa14ee211ef03f4b816da990ed60a4e08a0184a
Bruno Alves <93.bruno@gmail.com>
2024-05-25 18:15:22 +0100

    added Dialog component from material UI to confirm and remove the exercise

--- frontend/src/components/DeleteFromWorkoutButton.jsx
+++ frontend/src/components/DeleteFromWorkoutButton.jsx
@@ -1,11 +1,19 @@
 import { useState, useEffect } from "react";
-import { Button } from "@mui/material";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
 import { useLocation } from "wouter";
 import workoutServerCall from "../services/workoutsServerCall";
 
 function DeleteExerciseFromWorkoutButton({ exercise }) {
   const [location] = useLocation();
   const [workoutID, setWorkoutID] = useState(null);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     const workoutIdFromUrl = location.split("/").pop();
@@ -20,22 +28,49 @@ function DeleteExerciseFromWorkoutButton({ exercise }) {
           workoutID
         );
         console.log("Exercício removido do workout:", workoutID);
+        setOpen(false); // Fecha o diálogo após a remoção
       } catch (error) {
         console.error("Erro ao remover exercício do workout", error);
       }
     }
   }
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <Button
-      variant="contained"
-      size="small"
-      color="error"
-      sx={{ backgroundColor: "#ff2625" }}
-      onClick={handleDeleteExercise}
-    >
-      Remove from my workout
-    </Button>
+    <>
+      <Button
+        variant="contained"
+        size="small"
+        color="error"
+        sx={{ backgroundColor: "#ff2625" }}
+        onClick={handleClickOpen}
+      >
+        Remove from my workout
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Confirm Deletion</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Are you sure you want to remove this exercise from your workout?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleDeleteExercise} color="error">
+            Remove
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
 
